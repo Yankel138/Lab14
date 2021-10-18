4bcb797dec648519007e86b0d7aed75f5ca096eb
medzakovskiy.vi@yandex.ru <medzakovskiy.vi@yandex.ru>
2021-10-18 17:05:20 +0300

    Добавьте файлы проекта.

---
+++ Lab14/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cat cat = new Cat("Кот");
+            cat.ShowInfo();
+            Dog dog = new Dog("Пёс");
+            dog.ShowInfo();
+            Console.ReadKey();
+        }
+    }
+
+    abstract class Animal
+    {
+        public abstract string Name { set; get; }
+        public Animal(string name)
+        {
+            Name = name;
+        }
+        
+        public abstract void Say();
+
+        public void ShowInfo()
+        {
+            Console.WriteLine("Название животного: " + Name);
+            Console.Write(Name + " издает звук: ");
+            Say();
+        }
+
+
+
+    }
+
+    class Cat : Animal
+    {
+        string name;
+        public override string Name
+        {
+            set
+            {
+                this.name = value;
+            }
+            get
+            {
+                return name;
+            }
+        }
+
+        public Cat(string name)
+        : base(name)
+        {
+            Name = name;
+        }
+        public override void Say()
+        {
+            Console.WriteLine("Мяу");
+        }
+        public new void ShowInfo()
+        {
+            base.ShowInfo();
+        }
+
+    }
+
+    class Dog : Animal
+    {
+        string name;
+        public override string Name
+        {
+            set
+            {
+                this.name = value;
+            }
+            get
+            {
+                return name;
+            }
+        }
+        public Dog(string name)
+        : base(name)
+        {
+            Name = name;
+        }
+
+        public override void Say()
+        {
+            Console.WriteLine("Гав");
+        }
+        public new void ShowInfo()
+        {
+            base.ShowInfo();
+        }
+
+    }
+}
